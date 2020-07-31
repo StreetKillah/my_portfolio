03f20455d92fcd4b4baec781fbbf8b3104210780
Michael Prokopev <matroskin119@gmail.com>
2020-07-31 20:32:52 +0300

    the 1st

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+    $('.img_wrapper').css({'left': '0', 'transition':'0.5s linear 0s' });
+    $('.main_typo').css({'transition' : 'all 0.3s linear 0s', 'opacity' : '1'})
+
     $('.header_burger').click((e) =>{
-        $('.header_burger, .header, .header_menu, ul').toggleClass('active');
-       
-    })
+        $('.header_burger, .header, .header_content, ul, .header_list, .header_menu').toggleClass('active');
+    });
+
+    $('.header_list li:nth-child(2)').click((e) => {
+            window.scrollBy({top: 529,behavior: 'smooth'});
+    });
+    
 });
+
 
